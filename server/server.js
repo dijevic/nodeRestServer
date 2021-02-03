30f962e31c2aff6c8144e00123a1a2d96589fce6
[diego] <[dielmac7@gmail.com]>
2021-02-02 21:04:44 -0300

    npm start

--- server/server.js
+++ server/server.js
@@ -1,53 +1,50 @@
-require('./config/config');
-
+require('./config/config')
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
+const app = express()
 
-const bodyParser = require('body-parser');
 
-// parse application/x-www-form-urlencoded
+//middleware----- >
+ // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
+ 
 // parse application/json
 app.use(bodyParser.json())
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
-});
 
-app.post('/usuario', function(req, res) {
+app.get('/usuario', function (req, res) {
+    req
+  res.json('get usuario')
+})
 
+app.post('/usuario', function (req, res) {
     let body = req.body;
-
-    if (body.nombre === undefined) {
-
+    if(body.nombre === undefined){
         res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
+            ok:false,
+            status: 'bad request'+' '+ 400,
+            mensaje : 'algo salio mal, el nombre es necesario'
+        })
+    }else{
+        res.json({persona :body})
 
-    } else {
-        res.json({
-            persona: body
-        });
     }
+})
 
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id
     res.json({
         id
-    });
-});
+    })
+})
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+app.delete('/usuario', function (req, res) {
+    res.json('delete usuario')
+})
 
-app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto: ', process.env.PORT);
-});
+ 
+app.listen(3000,()=>{
+    console.log(`escuchando el puerto ${process.env.PORT}`)
+})
+
